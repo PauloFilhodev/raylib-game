688cee1c29eef24d1b2c20ad350ff61bc90ff74a
Paulo Filho <paulogamerzuero@gmail.com>
2026-09-04 18:12:49 -0300

    build: ajusta tasks.json para permitir compilação direta pelo vscode

--- src/main.c
+++ src/main.c
@@ -2,7 +2,15 @@
 
 int main ()
 {
-    InitWindow(800, 450, "Meu primeiro jogo");
+    InitWindow(800, 450, "Rayban");
+    SetTargetFPS(60);
+    
+    Rectangle player = {250, 250, 50, 50};
+    
+    float *player_pos_x = &player.x;
+    float *player_pos_y = &player.y;
+    float player_speed = 5.0f;
+    Rectangle player_passe = {*player_pos_x, *player_pos_y, 50, 50};
 
     while (!WindowShouldClose())
     {
@@ -10,13 +18,25 @@ int main ()
 
         ClearBackground(RAYWHITE);
 
-        DrawText(
-            "Raylib funcionando!",
-            250,
-            200,
-            30,
-            BLACK
-        );
+        if (IsKeyDown(KEY_W)) {
+            *player_pos_y -= player_speed;
+        }
+        if (IsKeyDown(KEY_S)) {
+            *player_pos_y += player_speed;
+        }
+        if (IsKeyDown(KEY_D)) {
+            *player_pos_x += player_speed;
+        }
+        if (IsKeyDown(KEY_A)) {
+            *player_pos_x -= player_speed;
+        }
+
+        if (IsKeyDown(KEY_B))
+        {
+            DrawRectangleRec(player_passe, BLUE);
+        }
+        
+        DrawRectangleRec(player, RED);
 
         EndDrawing();
     }
